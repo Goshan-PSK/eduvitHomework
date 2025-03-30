37b1a0a815e4941165ce956df8186bc5f5879cca
Dimon <jarvizbest@yandex.ru>
2025-03-30 15:04:21 +0300

    Word counter

---
+++ work_2/count_word.py
@@ -0,0 +1,11 @@
+text = input("Введите строку: ").lower()
+
+words = text.split()
+
+word_count = {}
+
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+
+for word, count in word_count.items():
+    print(f"{word}: {count}")
